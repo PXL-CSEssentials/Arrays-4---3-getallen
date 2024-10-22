e8d22a27e99ee6710316eeb1f795a81aef6c31e3
Koen Bloemen <koen.bloemen@pxl.be>
2024-10-22 17:11:54 +0200

    final

---
+++ Arrays-4---3-getallen/Program.cs
@@ -0,0 +1,32 @@
+﻿try
+{
+    const int NumberOfNumbers = 3;
+
+    Console.WriteLine($"==== Som bepalen van {NumberOfNumbers} getallen ====\r\n\r\n");
+    int[] numbers = new int[NumberOfNumbers];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"Geef getal {i + 1}/{NumberOfNumbers} : ");
+        numbers[i] = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+
+    //foreach (int number in numbers)
+    //    sum += number;
+
+    Console.WriteLine($"\r\nDe som is {sum}");
+    Console.ReadLine();
+}
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine($"Index bestaat niet!!!");
+    Console.ReadLine();
+}
